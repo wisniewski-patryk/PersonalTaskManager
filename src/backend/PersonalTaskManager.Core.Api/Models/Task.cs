914df7ac3632161c0367d54fd8fe18a7e75363e9
Patryk Wiśniewski <patryk.wisniewski@speeron.com>
2023-04-29 16:56:42 +0200

    add base entity

--- src/backend/PersonalTaskManager.Core.Api/Models/Task.cs
+++ src/backend/PersonalTaskManager.Core.Api/Models/Task.cs
@@ -1,6 +1,18 @@
-public class Task
-{
-    public Guid Id { get; set; } 
+public class Task : BaseEntity
+{ 
+    public Task() : base()
+    {
+    }
+    public Task(string name) : base()
+    {
+        this.Name = name;
+    }
+
+    public Task(string name, string description) : base()
+    {
+        this.Name = name;
+        this.Description = description;
+    }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
 }
